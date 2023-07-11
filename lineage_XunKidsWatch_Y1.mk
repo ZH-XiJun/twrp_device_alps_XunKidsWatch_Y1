ff3b4e09de10c4a718a7386ce87681eaf4935bf1
wt_bruhhhh <59949135+ZH-XiJun@users.noreply.github.com>
2023-07-11 23:57:38 +0800

    edit for lineageos

--- lineage_XunKidsWatch_Y1.mk
+++ lineage_XunKidsWatch_Y1.mk
@@ -1,6 +1,5 @@
 #
 # Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,14 +7,14 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common LineageOS stuff.
+# $(call inherit-product, vendor/lineage/config/common.mk)
 
 # Inherit from XunKidsWatch_Y1 device
 $(call inherit-product, device/alps/XunKidsWatch_Y1/device.mk)
 
 PRODUCT_DEVICE := XunKidsWatch_Y1
-PRODUCT_NAME := omni_XunKidsWatch_Y1
+PRODUCT_NAME := lineage_XunKidsWatch_Y1
 PRODUCT_BRAND := Xiaoxun
 PRODUCT_MODEL := XunKidsWatch_Y1
 PRODUCT_MANUFACTURER := alps
